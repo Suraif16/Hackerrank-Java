4a53b53c7cbb3cff9dbb8ff3abd3029e17514020
Suraif Muhammad <muhammadsuraif2000@gmail.com>
2023-01-02 14:23:07 +0530

    add solution2

---
+++ Solution2.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Solution2 {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if(N>=1 && N<=100)
+        {
+                if(N%2 == 1)
+                {
+                    System.out.print("Weird");
+                }
+                else if(N<=5)
+                {
+                    System.out.print("Not Weird");
+                }
+                else if(N<=20)
+                {
+                    System.out.print("Weird");
+                }
+                else 
+                {
+                    System.out.print("Not Weird");
+                }
+        }
+        else
+        {
+            System.out.println("Invalid Input");
+        }
+        scanner.close();
+    }
+}
