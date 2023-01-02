1f1eadb0be968071182d04068ce2c6c4ed53ee5a
Suraif Muhammad <muhammadsuraif2000@gmail.com>
2023-01-02 14:09:35 +0530

    add Solution1

---
+++ Solution1.java
@@ -0,0 +1,30 @@
+public class Solution1 //A class name should always start with Capitals, Java is class sensitive
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+        System.out.println(8*2);
+        String var1 = "Suraif";
+        System.out.println(var1);
+    }
+}
+/*Every java program has a class name which must match the file name and that every program must contain the main() method */
+//System is a built in java class that contains , out (which is short of output).
+
+/*
+ * Java variables* - Primitive Data types
+ * byte
+ * Short
+ * int
+ * float
+ * double
+ * char
+ * boolean
+ * *Non-primitive data types**
+ * String
+ * Array
+ * Class
+ * 
+ */
+
